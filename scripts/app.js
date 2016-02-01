cd4254993b5ecd559dae1ceaa0e15cf668cbabd5
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-01 15:34:04 -0800

    built logic that populates index.html with relevant projects using jQuery logic and object constructor

--- scripts/app.js
+++ scripts/app.js
@@ -11,9 +11,17 @@ Project.prototype.toHtml = function () {
   var $newProject = $('article.template').clone();
   $newProject.attr('data-category', this.category);
   $newProject.find('h2').text(this.title);
-  $newProject.find('time').html('about' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + 'days ago');
+  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newProject.find('.project-body').html(this.body);
   $newProject.append('<hr>');
   $newProject.removeClass();
   return $newProject;
 };
+
+rawData.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
+
+projects.forEach(function(a){
+  $('#projects').append(a.toHtml());
+});
