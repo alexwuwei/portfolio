5970954d569ba22a70e5e82cfd9cbda3237ac6c0
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-02 21:58:28 -0800

    enabled show-hide functionality, using menu bar event attributes to display only desired section

--- scripts/viewRender.js
+++ scripts/viewRender.js
@@ -4,6 +4,10 @@ projectView.handleMainNavBar = function () {
   $('.main-nav-bar').on('click', 'li', function (event) {
     event.preventDefault();
     $('.nav-content').hide();
+    var $dataType = $(this).data('content');
+    console.log($dataType);
+    console.log(this);
+    $('#' + $dataType).show();
   });
 };
 
