67659bb88c3acec16ddf0562663e7511af69c3ef
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-09 16:28:33 -0800

    wrapped all viewRender.js functionality in IIFE

--- scripts/viewRender.js
+++ scripts/viewRender.js
@@ -1,21 +1,25 @@
-var projectView = {};
+(function(module) {
 
-projectView.handleMainNavBar = function () {
-  $('.main-nav-bar').on('click', 'li', function (event) {
-    event.preventDefault();
-    $('.nav-content').hide();
-    var $dataType = $(this).data('content');
-    console.log($dataType);
-    console.log(this);
-    $('#' + $dataType).fadeIn('slow');
-  });
-};
+  var projectView = {};
 
-projectView.initIndexPage = function () {
-  Project.all.forEach(function (a) {
-    $('#projects').append(a.toHtml());
-  });
-};
+  projectView.handleMainNavBar = function () {
+    $('.main-nav-bar').on('click', 'li', function (event) {
+      event.preventDefault();
+      $('.nav-content').hide();
+      var $dataType = $(this).data('content');
+      console.log($dataType);
+      console.log(this);
+      $('#' + $dataType).fadeIn('slow');
+    });
+  };
+
+  projectView.initIndexPage = function () {
+    Project.all.forEach(function (a) {
+      $('#projects').append(a.toHtml());
+    });
+  };
 
 
-projectView.handleMainNavBar();
+  projectView.handleMainNavBar();
+  module.projectView = projectView;
+})(window);
