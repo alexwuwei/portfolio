463c6c8f86231ad0eab71178a3c7bbfbb813828c
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-16 14:44:06 -0800

    added initial functionality to repo.js, test pending

---
+++ scripts/repo.js
@@ -0,0 +1,23 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function (callback) {
+    $.ajax ({
+      url: 'https://api.github.com/users/alexwuwei/repos' + '?per_page=7&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data, message, xhr) {
+        repos.all = data;
+      }
+    }).done(callback);
+  };
+
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo){
+      return repo[attr];
+    });
+  };
+  module.repos = repos;
+})(window);
