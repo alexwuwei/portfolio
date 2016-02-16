e5103e22ed53134ea8a27fbac8432c8e60c758df
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-16 14:53:09 -0800

    added initial functionality for repoView.js, testing pending

--- scripts/repoRender.js
+++ scripts/repoRender.js
@@ -1,3 +1,22 @@
 (function(module){
-  
+  var repoView = {};
+
+  var ui = function() {
+    var $about = $('#about');
+
+    $about.find('section').empty();
+    $about.show().siblings().hide();
+  };
+
+  var render = function(repo) {
+    return $('<li>').html('<a href="' + repo.html_url + '">' + repo.name +'</a>');
+  };
+
+  repoView.index = function () {
+    ui();
+    $('#about').append(
+      repos.with('name').map(render)
+    );
+  };
+  module.repoView = repoView;
 })(window);
