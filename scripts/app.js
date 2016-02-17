7a2091a2042328ad117dcfc50ab0add7dc220319
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-17 15:52:05 -0800

    removed app.js comments per TA instructions

--- scripts/app.js
+++ scripts/app.js
@@ -9,15 +9,6 @@ function Project (opts) {
 }
 
 Project.prototype.toHtml = function () {
-  // var $newProject = $('article.template').clone();
-  // $newProject.attr('data-category', this.category);
-  // $newProject.find('h2').text(this.title);
-  // $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-  // $newProject.find('.project-body').append(this.image);
-  // $newProject.find('.project-body').append(this.body);
-  // $newProject.append('<hr>');
-  // $newProject.removeClass();
-  // return $newProject;
   var template = Handlebars.compile($('#project-template').text());
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published' + this.daysAgo + ' days ago' : '(draft)';
